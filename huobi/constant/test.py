e5880881db85835093cfb1ec23426596c9fb2240
devin-y <devin0thinking@gmail.com>
2019-11-02 20:47:04 +0800

    add test config

--- huobi/constant/test.py
+++ huobi/constant/test.py
@@ -1,4 +1,13 @@
-g_api_key="yyyyyy"
-g_secret_key="xxxxxx"
+import os
+if(os.path.exists("huobi/privateconfig.py")):
+    from huobi.privateconfig import *
+    g_api_key = p_api_key
+    g_secret_key = p_secret_key
+else:
+    g_api_key="yyyyyy"
+    g_secret_key="xxxxxx"
 
 g_account_id = 12345678
+
+
+
