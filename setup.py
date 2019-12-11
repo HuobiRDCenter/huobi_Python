e9c42956f640d1b16f620be2bee5f61708c5bf4e
devin-y <devin0thinking@gmail.com>
2019-12-11 14:16:12 +0800

    change client version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="huobi-client",
-    version="1.0.6",
+    version="1.0.8",
     packages=['huobi', 'huobi.impl', 'huobi.impl.utils', 'huobi.exception', 'huobi.model', 'huobi.base', 'huobi.constant',
               "performance", "tests", "huobi.demo_service", "huobi.demo_service.mbp"],
     install_requires=['requests', 'apscheduler', 'websocket-client', 'urllib3']
