92b96679f6e239c785df7c4354a0a94deda2768f
eynzhang <2427928+eynzhang@users.noreply.github.com>
2020-06-30 21:15:00 +0800

    Update version to 2.0.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="huobi-client",
-    version="2.0.1",
+    version="2.0.0",
     packages=['huobi',
               'huobi.exception', 'huobi.constant',
               'huobi.utils',
