268e7aa462cd0f14ceef53343a49a36ed2515619
tony.ren <ren15588419619@163.com>
2024-10-17 22:13:54 +0800

    ed25519签名

--- huobi/model/etf/__init__.py
+++
@@ -1,4 +0,0 @@
-from huobi.model.etf.etf_swap_config import EtfSwapConfig
-from huobi.model.etf.etf_swap_list import EtfSwapList
-from huobi.model.etf.etf_swap_in_out import EtfSwapInOut
-from huobi.model.etf.unitprice import UnitPrice
--- tests/main.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "UT test start ..."
-python3 -m unittest test_api_signature.TestApi
-echo "UT test end ..."
